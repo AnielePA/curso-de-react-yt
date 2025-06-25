b96e782a03f63f22d02cca60652fd1ac58869757
aniele <ardissonaniele99@gmail.com>
2025-06-25 10:44:15 -0400

    enviando projeto react completo

---
+++ src/App.jsx
@@ -0,0 +1,103 @@
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import { useEffect, useState } from "react";
+import { v4 } from "uuid";
+import Title from "./components/Title";
+
+function App() {
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || [],
+
+    [
+      {
+        id: 1,
+        title: "Estudar programação",
+        description: "Estudar para a prova e o desafio",
+        completed: false,
+      },
+
+      {
+        id: 2,
+        title: "Fazer exercícios",
+        description: "Fazer exercicios de tarde",
+        completed: false,
+      },
+
+      {
+        id: 3,
+        title: "Compras",
+        description: "123",
+        completed: false,
+      },
+    ]
+  );
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    async function fetchTasks() {
+      //chama api
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos?_limit=10",
+        {
+          method: "GET",
+        }
+      );
+
+      //pega os dados que ela armezena
+      const data = await response.json();
+
+      //armazenar(persistir) esses dados no state
+      setTasks(data);
+    }
+    // fetchTasks();
+    //se quiser obter as tasks da api, descomenta essa linha acima
+  }, []);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      //preciso atualizar a tarefa
+      if (task.id === taskId) {
+        return { ...task, completed: !task.completed };
+      }
+
+      //não preciso atualizar essa tarefa
+      return task;
+    });
+    //atualizando estado
+    setTasks(newTasks);
+  }
+
+  function onDeleteTask(taskId) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  }
+
+  function onAddTaskSubmit(title, description) {
+    const newTasks = {
+      id: v4(),
+      title,
+      description,
+      completed: false,
+    };
+    setTasks([...tasks, newTasks]);
+  }
+
+  return (
+    <div className='w-screen h-screen bg-late-500 flex justify-center p-6 '>
+      <div className='w-[500px] space-y-4'>
+        <Title>Gerenciador de tarefas</Title>
+        <AddTask onAddTaskSubmit={onAddTaskSubmit} />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTask={onDeleteTask}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,44 @@
+import { ChevronsRightIcon, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import Button from "./button";
+
+function Tasks(props) {
+  // console.log(props);
+  const navigate = useNavigate();
+
+  function onSeeDetailsClick(task) {
+    const query = new URLSearchParams();
+    query.set("title", task.title);
+    query.set("description", task.description);
+    navigate(`/task?${query.toString()}`);
+  }
+
+  return (
+    <ul className='space-y-4 p-6 bg-slate-200 rounded-md shadow'>
+      {props.tasks.map((task) => (
+        <li key={task.id} className='flex gap-2'>
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className={`bg-slate-400 text-left text-white p-2 rounded-md w-full ${
+              task.completed && "line-through"
+            }`}
+          >
+            {task.title}
+          </button>
+          <Button
+            onClick={() => onSeeDetailsClick(task)}
+          >
+            <ChevronsRightIcon />
+          </Button>
+          <Button
+            onClick={() => props.onDeleteTask(task.id)}
+          >
+            <TrashIcon />
+          </Button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Tasks;
