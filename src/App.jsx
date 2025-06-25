ada83db400cffab27a01950381e1977efbf4e6cf
aniele <ardissonaniele99@gmail.com>
2025-06-25 11:11:07 -0400

    Corrigindo detalhes de design

--- src/App.jsx
+++ src/App.jsx
@@ -86,7 +86,7 @@ function App() {
   }
 
   return (
-    <div className='w-screen h-screen bg-late-500 flex justify-center p-6 '>
+    <div className='w-screen h-screen bg-slate-500 flex justify-center p-6 '>
       <div className='w-[500px] space-y-4'>
         <Title>Gerenciador de tarefas</Title>
         <AddTask onAddTaskSubmit={onAddTaskSubmit} />
